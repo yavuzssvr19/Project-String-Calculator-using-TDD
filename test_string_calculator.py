8acc093883680b5543507df1b948a43576af0b21
Yavuz Selim Sever <yavuzssvr19@hotmail.com>
2024-02-08 02:06:49 +0300

    first commit - commiting all the files to repository

---
+++ test_string_calculator.py
@@ -0,0 +1,56 @@
+import unittest
+
+class StringCalculator:
+    @staticmethod
+    def add(numbers):
+        # Eğer string boş ise, 0 dön
+        if not numbers:
+            return 0
+
+        # Farklı ayırıcıları destekleme
+        delimiter = ","
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1]
+
+        # Ayırıcıları standart hale getir (virgül ve yeni satır)
+        numbers = numbers.replace("\n", delimiter)
+        number_list = numbers.split(delimiter)
+
+        # Negatif sayı kontrolü
+        negatives = [int(x) for x in number_list if int(x) < 0]
+        if negatives:
+            raise Exception(f"negatives not allowed: {', '.join(map(str, negatives))}")
+
+        # Sayıları topla
+        return sum(int(x) for x in number_list if x.isdigit())
+    print(add("1,-2,3"))
+
+class TestStringCalculator(unittest.TestCase):
+
+    def test_empty_string_returns_zero(self):
+        self.assertEqual(StringCalculator.add(""), 0)
+        print("sıkıntı yok")
+
+    def test_single_number_returns_value(self):
+        self.assertEqual(StringCalculator.add("1"), 1)
+
+    def test_two_numbers_comma_delimited_returns_sum(self):
+        self.assertEqual(StringCalculator.add("1,2"), 3)
+
+    def test_handle_new_lines_as_delimiters(self):
+        self.assertEqual(StringCalculator.add("1\n2,3"), 6)
+
+    def test_support_different_delimiters(self):
+        self.assertEqual(StringCalculator.add("//;\n1;2"), 3)
+
+    def test_negative_numbers_throw_exception(self):
+        with self.assertRaises(Exception) as context:
+            StringCalculator.add("-1,2")
+        self.assertTrue("negatives not allowed" in str(context.exception))
+
+       
+
+
+
